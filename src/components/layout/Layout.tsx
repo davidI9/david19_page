962b22c0a1b6e031fca5bf908cc385fecea9c11b
David19 <davidsanchezpalomino200603@gmail.com>
2025-09-03 11:06:34 +0200

    fixed: menu

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -85,11 +85,11 @@ const Header = () => {
     const [open, setOpen] = useState(false)
 
     return (
-        <div id="start" className="border-b-1 border-gray-900 w-full">
-            <div className=" grid lg:grid-cols-3 md:grid-cols-3 grid-cols-2">
-                <h1 className="text-white font-mono text-s md:text-lg lg:text-3xl justify-self-center p-7">&lt;David19_/&gt;</h1>
+        <div id="start" className="fixed top-0 border-b-1 border-gray-900 w-full bg-gradient-to-b from-black to-transparent">
+            <div onClick={open ? (() => setOpen(!open)): (() => setOpen(!open))} className=" grid lg:grid-cols-3 md:grid-cols-3 grid-cols-2">
+                <h1 className="text-white font-mono text-s md:text-lg lg:text-3xl justify-self-center p-7 md:pl-33">&lt;David19_/&gt;</h1>
                 <div className="hidden md:block"></div>
-                <div className="ml-20 lg:max-w-15 lg:max-h-15 max-w-10 md:max-h-10 justify-self-center self-center">
+                <div className="ml-20 lg:max-w-15 lg:max-h-15 max-w-10 md:max-h-10 justify-self-center md:mr-10 self-center">
                     <button onClick={() => setOpen(!open)}>
                         <img src="https://i0.wp.com/benjaminmarcello.nl/wp-content/uploads/2021/12/menu-icon-PNG-1.png?fit=500%2C500&ssl=1" alt="menuicon"/>
                         {open ? "" : ""}
