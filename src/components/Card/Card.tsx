ed7abcf66c95c5f2b4a4534ca5545095df934f9a
David19 <davidsanchezpalomino200603@gmail.com>
2025-11-15 19:05:05 +0100

    mas guapa todavia

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -20,16 +20,16 @@ export const Card = () => {
             <div id="about-me" className={styles.centered3}>
                 <div className="mt-30 max-w-7/8 shadow-lg shadow-gray-900 justify-self-center ">
                     <p className="text-white font-mono tracking-widest text-xs md:text-s lg:text-xl justify-self-start pb-2 border-b-2 mb-5 border-gray-200">&lt;ABOUT ME/&gt;</p>
-                    <p className="text-gray-400 text-xs md:text-s lg:text-xl outline-1 outline-stone-900 justify-self-start p-2">
+                    <p className="text-gray-200 text-xs md:text-s lg:text-xl outline-1 outline-stone-900 justify-self-start p-2">
                         Hi there, my name is David...
                         <br/><br/>
                         Started building back in 2023 with a few python docs, loved it but didn't really put much effort. Later in 2024, I started to take it seriously and here I am now!
                         <br/><br/>
                         Since 2024, I am a Software Engineering student in Universidad de Córdoba. Here, I ve participated in lots of activities related to my knowledge area, most of them
-                        impulsed by the UCO's students organization called <a href="https://www.uco.es/aulasoftwarelibre/" target="blank" className="text-white hover:text-blue-300">Aula de Software Libre</a> (Free Software Classroom).
+                        impulsed by the UCO's students organization called <a href="https://www.uco.es/aulasoftwarelibre/" target="blank" className="text-[#a5945a] hover:text-[#cac0b5]">Aula de Software Libre</a> (Free Software Classroom).
                         <br/><br/>
                         I've participated so much that they gave me a spot in the team of coordination, in order to help with the organization of the activities.
-                        I suggest you to check out some of the ASL's iniciatives such as the <a href="https://salmorejo.tech/2025/" target="blank" className="text-white hover:text-red-300">SalmorejoTech</a> event!
+                        I suggest you to check out some of the ASL's iniciatives such as the <a href="https://salmorejo.tech/2025/" target="blank" className="text-[#a5945a] hover:text-[#cac0b5]">SalmorejoTech</a> event!
                         <br/><br/>
                         For now I am keen on keeping up with the pace of the career while I develop my skills on software development.
                         <br/><br/>
@@ -38,14 +38,14 @@ export const Card = () => {
             </div>
 
             <div id="skills" className={styles.centered3}>
-                <div className="mt-30 max-w-7/8 shadow-lg shadow-gray-900 justify-self-center">
+                <div className="mt-30 max-w-7/8 shadow-lg shadow-gray-900 justify-self-center ">
                     <p className="text-white font-mono tracking-widest text-xs md:text-s lg:text-xl justify-self-start pb-2 border-b-2 mb-5 border-gray-200">&lt;SKILLS/&gt;</p>
-                    <p className="text-gray-400 text-xs md:text-s lg:text-xl outline-1 outline-stone-900 justify-self-start p-2">
-                        Among some of my skills, I am used to work with <text className="text-white text-xs md:text-s lg:text-xl">Python, C, C++, Javascript/Typescript</text> and the <text className="text-white text-xs md:text-s lg:text-xl">CSS+HTML</text> webpack, also I ve tasted  <text className="text-white text-xs md:text-s lg:text-xl">React, Tailwindcss, Django</text> and some other frameworks.
+                    <p className="text-gray-200 text-xs md:text-s lg:text-xl outline-1 outline-stone-900 justify-self-start p-2">
+                        Among some of my skills, I am used to work with <text className="text-[#a5945a] text-xs md:text-s lg:text-xl">Python, C, C++, Javascript/Typescript</text> and the <text className="text-[#a5945a] text-xs md:text-s lg:text-xl">CSS+HTML</text> webpack, also I ve tasted  <text className="text-[#a5945a] text-xs md:text-s lg:text-xl">React, Tailwindcss, Django</text> and some other frameworks.
                         <br/><br/>
-                        I am also experienced in <text className="text-white text-xs md:text-s lg:text-xl">clean architecture principles and DDD design</text>, I have some projects which implements all the previous mentioned knowledge in my github. 
+                        I am also experienced in <text className="text-[#a5945a] text-xs md:text-s lg:text-xl">clean architecture principles and DDD design</text>, I have some projects which implements all the previous mentioned knowledge in my github. 
                         <br/><br/>
-                        But I must say that my most powerfull traits are my <text className="text-white text-xs md:text-s lg:text-xl">proactivity and leadership</text>, I am always thinking on things to do in order to succed in what I want to achieve,
+                        But I must say that my most powerfull traits are my <text className="text-[#a5945a] text-xs md:text-s lg:text-xl">proactivity and leadership</text>, I am always thinking on things to do in order to succed in what I want to achieve,
                         always trying to take a logical perspective.
                         <br/><br/>
                         I am leaving you my social media and github below, where you'll be able to find some of the things I've been doing, there are so many things coming in the future... Hope you like my work and feel free to contact me for anything!
