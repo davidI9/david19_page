f7a5c32e252f039c24eb86eedb306c1f60126321
David19 <davidsanchezpalomino200603@gmail.com>
2026-02-16 21:32:46 +0100

    cat fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Layout } from './components/Layout/Layout'
+//import { Layout } from './components/Layout/Layout'
 import { Down } from './down'
 
-//uncoment when web is ready and comment the other createRoot
+//uncoment when web is ready and comment the other createRoot also uncomment the layout import
 /* createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Layout/>
