e75ccbe6f14bae7a426328c4a77ba43a35297a5e
David19 <davidsanchezpalomino200603@gmail.com>
2026-02-21 11:23:37 +0100

    background design

---
+++ src/components/Landing/Landing-layout.tsx
@@ -0,0 +1,17 @@
+import styles from "./Landing.module.css"
+
+export const LandingLayout = () => {
+    return (
+        <div className={`${styles.firstlayer} w-full h-screen aspect-square`}>
+            <div className={`rotate-90 fixed ${styles.text} top-60 left-[-20vw]`}>
+                <p className="text-gray-600">19</p><p>DAVID</p>
+            </div>
+
+            <div className={`${styles.secondlayer} w-full h-screen aspect-square`}>
+                <div className={`${styles.thirdlayer} w-full h-screen`}>
+            
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LandingLayout } from './components/Landing/Landing-layout'
 //import { Layout } from './components/Layout/Layout'
-import { Down } from './down'
+//import { Down } from './down'
 
-//uncoment when web is ready and comment the other createRoot also uncomment the layout import
-/* createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Layout/>
-  </StrictMode>,
-) */
-
+//uncomment when web is ready and comment the other createRoot also uncomment the layout import
 createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <LandingLayout/>
+  </StrictMode>
+)
+
+//uncomment whenever the page is under maintainance, also uncomment the down import 
+/* createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Down/>
   </StrictMode>,
-)
+)*/
