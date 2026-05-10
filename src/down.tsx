87d5dd8da2b931a2d1cb1a67b45efe6c7d215f31
David19 <davidsanchezpalomino200603@gmail.com>
2026-05-10 19:47:32 +0200

    down version updated

--- src/down.tsx
+++ src/down.tsx
@@ -1,11 +1,18 @@
 export const Down = () => {
     return (
         <div>
-            <div className="grid grid-rows-2 justify-center w-full bg-green-400 h-screen">
-                <p className="content-center">
+            <div className="grid grid-rows-3 justify-center bg-green-400 min-h-screen w-full">
+                <p className="content-center justify-self-center text-2xl font-mono tracking-widest text-gray-900 w-[90%] mt-20">
                     estoy chetando la pagina // the page is horrendous and I am working on it!
                 </p>
-                <img src="/images/cat-computer.gif" alt="gato" />
+                <div className="grid grid-cols-2 gap-20 w-[90%] justify-self-center">
+                    <div className="max-w-50 max-h-50 bg-transparent justify-self-center">
+                        <a href="https://github.com/davidI9" target="blank">
+                            <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-white-icon.png" alt="ig"/>
+                        </a>
+                    </div>
+                    <img src="/images/cat-computer.gif" alt="gato" />
+                </div>
             </div>
         </div>
     )
